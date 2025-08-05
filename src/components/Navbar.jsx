7085de679bb492561e05e08c0a3aa89d5aa0aa5a
Sshibam <shibam.hi@gmail.com>
2025-08-06 00:35:54 +0530

    logo-color-&-name-modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
       <nav className="bg-black bg-opacity-50 text-white p-4 flex justify-between sticky top-0 z-50">
-        <h1 className="text-xl font-bold">HOPE</h1>
+        <h1 className="text-xl font-bold text-emerald-300">HOPE.org</h1>
         <div className="space-x-4 text-base">
           <a href="#home">Home</a>
           <a href="#about">About</a>

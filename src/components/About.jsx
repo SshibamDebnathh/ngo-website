6cabf6be4952f13f7676a55c572cd95b18a1ad9b
Sshibam <shibam.hi@gmail.com>
2025-08-06 00:39:13 +0530

    minor-html-edit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,11 @@ export default function About() {
   return (
     <>
     <div className="flex flex-wrap lg:flex-nowrap justify-center p-5 items-center bg-gray-300 w-full">
-      <section className="p-8 lg:w-1/2 w-full justify-center items-center flex-row bg-gray-500 text-white m-3 rounded-lg border-black border-2 lg:h-[435px]">
+      <section id="about" className="p-8 lg:w-1/2 w-full justify-center items-center flex-row bg-gray-500 text-white m-3 rounded-lg border-black border-2 lg:h-[435px]">
       <h2 className="text-3xl font-bold text-center m-8">About Us</h2>
       We are a passionate team committed to creating positive change in society. Through community outreach, education, and volunteer efforts, we strive to empower the underprivileged and support sustainable development. Our mission is to bring people together and make a lasting impact through compassion, dedication, and meaningful action.
       </section>
-    <section id="about" className="p-5 bg-gray-300 lg:w-1/2 w-full m-3">
+    <section  className="p-5 bg-gray-300 lg:w-1/2 w-full m-3">
       <h2 className="text-3xl font-bold text-center m-8">What we do</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-4xl mx-auto">
         <div className="bg-emerald-500 text-white p-5 h-44 flex-row items-center justify-center text-center rounded shadow-md border-gray-200 border-2">
